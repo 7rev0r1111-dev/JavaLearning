2570557aa9dd32d57ca0315b0b7fc37593382440
Trevor <7rev0r1111@gmail.com>
2026-09-16 12:58:48 +0800

    Practic branch change

---
+++ Lesson9.java
@@ -0,0 +1,13 @@
+public class Lesson9 {
+    public static void main(String[] args) {
+        int result = doubleNumber(7);
+        System.out.println(result);
+    }
+
+    public static int doubleNumber(int number) {
+        return number * 2;
+    }   
+}
+
+
+// pracitce branch change
